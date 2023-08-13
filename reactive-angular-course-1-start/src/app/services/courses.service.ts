3171885c3d709db0bfb3e277cd167b04d004e2fe
Phits <john@fitz.com>
2023-08-13 13:22:04 -0400

    Implemented Single Data Observable Pattern

--- reactive-angular-course-1-start/src/app/services/courses.service.ts
+++ reactive-angular-course-1-start/src/app/services/courses.service.ts
@@ -1,18 +1,38 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Course} from '../model/course';
-import {map, shareReplay} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { map, shareReplay } from 'rxjs/operators';
 import { Lesson } from '../model/lesson';
 
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
 export class CoursesService {
 
-    constructor(private http:HttpClient) {
+    constructor(private http: HttpClient) {
 
+    }
+
+    loadCourseById(courseId: number) {
+        return this.http.get<Course>(`/api/courses/${courseId}`)
+            .pipe(
+                shareReplay()
+            )
+    }
+
+    loadAllCoursesById(courseId: number): Observable<Lesson[]> {
+        return this.http.get<Lesson[]>('/api/lessons', {
+            params: {
+                pageSize: "10000",
+                courseId: courseId.toString()
+            }
+        })
+            .pipe(
+                map(res => res["payload"]),
+                shareReplay()
+            );
     }
 
     loadAllCourses(): Observable<Course[]> {
@@ -23,14 +43,14 @@ export class CoursesService {
             );
     }
 
-    saveCourse(courseId: string, changes: Partial<Course>):Observable<any> {
+    saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
         return this.http.put(`/api/courses/${courseId}`, changes)
-        .pipe(
-            shareReplay()
-        )
+            .pipe(
+                shareReplay()
+            )
     }
 
-    searchLessons(search:string): Observable<Lesson[]> {
+    searchLessons(search: string): Observable<Lesson[]> {
         return this.http.get<Lesson[]>('/api/lessons', {
             params: {
                 filter: search,
